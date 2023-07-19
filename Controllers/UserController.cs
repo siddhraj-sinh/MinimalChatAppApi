c3f25ba8fbafc9145e63b515f2bd4f4de2c4eb7d
siddhraj-sinh <skrathod282001@gmail.com>
2023-07-19 15:59:38 +0530

    added register action method

---
+++ Controllers/UserController.cs
@@ -0,0 +1,214 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalChatAppApi.Data;
+using MinimalChatAppApi.Models;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MinimalChatAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ChatContext _context;
+        
+        private readonly IConfiguration _configuration;
+        public UserController(ChatContext context,IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+        [HttpPost("/api/register")]
+        public async Task<ActionResult<RegisterResponseDto>> Register(RegisterRequestDto user) {
+
+
+            if (await _context.Users.AnyAsync(u => u.Email == user.Email))
+            {
+                return Conflict("Email is already registered");
+            }
+
+            // Create a new user
+            var newUser = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = EncodePassword(user.Password)
+            };
+
+            // Save the user to the database
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            // Return the user's information in the response body
+            var response = new RegisterResponseDto
+            {
+                UserId = newUser.Id,
+                Name = newUser.Name,
+                Email = newUser.Email
+            };
+
+            return Ok(response);
+
+        }
+
+
+        //  hash the password
+        private string EncodePassword(string password)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+            string hash = BCrypt.Net.BCrypt.HashPassword(password, salt);
+            Console.WriteLine("hash",hash);
+            return hash;
+        }
+
+        //[HttpPost("/api/login")]
+        //public async Task<ActionResult<User>> Login(LoginRequestDto loginDto) {
+        //    var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email);
+
+        //    //check email
+        //    if (user == null) return Unauthorized("Invalid Credentails");
+
+
+        //    //verify password
+        //    bool isPasswordValid = DecodePassword(loginDto.Password, user.Password);
+        //    if (!isPasswordValid)
+        //    {
+        //        return Unauthorized("Invalid credentials");
+        //    }
+
+        //    var token = GenerateJwtToken(user);
+
+        //    // Construct the response body
+        //    var responseDto = new LoginResponseDto
+        //    {
+        //        Token = token,
+        //        Profile = new UserProfileDto
+        //        {
+        //            UserId = user.Id,
+        //            Name = user.Name,
+        //            Email = user.Email
+        //        }
+        //    };
+        //    return Ok(responseDto);
+        //}
+
+        //public bool DecodePassword(string password,string hashPassword) {
+        //    return BCrypt.Net.BCrypt.Verify(password, hashPassword);
+        //}
+
+        //// Helper method to generate a JWT token
+        //private string GenerateJwtToken(User user)
+        //{
+        //    var claims = new[]
+        //    {
+        //        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        //        new Claim(ClaimTypes.Name, user.Name),
+        //        new Claim(ClaimTypes.Email, user.Email)
+        //    };
+        //    string _jwtSecret = _configuration.GetSection("AppSettings:Token").Value;
+        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSecret ));
+        //    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        //    var token = new JwtSecurityToken(
+        //        //issuer: "YourIssuer",
+        //        //audience: "YourAudience",
+        //        claims: claims,
+        //        expires: DateTime.UtcNow.AddDays(3), 
+        //        signingCredentials: credentials
+        //    );
+
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+        //// GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/User/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/User
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/UserProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalChatAppApi.Models
+{
+    public class UserProfileDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
